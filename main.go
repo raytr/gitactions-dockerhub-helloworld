16d153baa039102f4b868046eff4b94420ca06be
nghiatran <nghia.tran@f6galaxy.com>
2022-08-25 18:37:00 +0700

    dockerfile

--- main.go
+++ main.go
@@ -5,26 +5,13 @@ import (
 	"net/http"
 )
 
-func hello(w http.ResponseWriter, req *http.Request) {
-
-	fmt.Fprintf(w, "hello\n")
-}
-
-func headers(w http.ResponseWriter, req *http.Request) {
-
-	for name, headers := range req.Header {
-		for _, h := range headers {
-			fmt.Fprintf(w, "%v: %v\n", name, h)
-		}
-	}
+func helloworld(w http.ResponseWriter, req *http.Request) {
+	w.Write([]byte("Github actions Docker hub Hello World"))
 }
 
 func main() {
 	fmt.Println("Start app")
-
-	http.HandleFunc("/hello", hello)
-	http.HandleFunc("/headers", headers)
-
+	http.HandleFunc("/", helloworld)
 	fmt.Println("listening on 8090")
 	http.ListenAndServe(":8090", nil)
 }
